c936e688cc88de28154eac0aa34294859f886881
Maxime Guedj <max.guedj13@gmail.com>
2016-08-11 15:26:47 +0200

    Minor bug fix

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ const docApiGenerator = require('../');
 describe('Testing doc-api-generator', function () {
   it('Should generate a documentation', function (done) {
 
-    docApiGenerator.generateDocumentation(path.join(__dirname, '/fixture/api-service.yaml'),
+    docApiGenerator.generateDocumentation(path.join(__dirname, '/fixture/dev-service.yaml'),
       path.join(__dirname, '/fixture/CHANGELOG.md'),
       '1.0.0',
       path.join(__dirname, '/result'),
